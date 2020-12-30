da0e19c9712a6f2386523b59ed14be8187f16d46
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-29 22:14:44 -0600

    commit tree 802cd05a175f5c8c9fb48df6cd69041324009834

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,26 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
+
+        erb :super_hero
+    end
+
+    post '/team' do 
+        @team_name = params[:team][:name]
+        @team_motto = params[:team][:motto]
+        @hero_name = []
+        @hero_power = []
+        @hero_bio = []
+        @hero_members = params[:team][:members]
+        @hero_members.each do |hero|
+            @hero_name << hero[:name]
+            @hero_power << hero[:power]
+            @hero_bio << hero[:biography]
+        end
+
+        erb :team
+    end
+
 
 end
